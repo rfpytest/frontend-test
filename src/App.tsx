6b0b80d3d489a4576be8028e1d22eea2a802e75d
rfpytest <rodrigofepy+test@gmail.com>
2022-08-26 00:05:18 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Autocomplete from "./components/Autocomplete";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world!!!!!</h1>
+      <Autocomplete />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Autocomplete = () => <input />;
+
+export default Autocomplete;
